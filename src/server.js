4d0f32d335bb777bbe5b15f76ebf34781d145192
mukti222 <135799527+mukti222@users.noreply.github.com>
2023-11-14 14:25:19 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const routes = require('./routes');
 // import routes from './routes';
  
 const init = async () => {
-  constserver = Hapi.server({    port: 5000,
+  const server = Hapi.server({    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
         cors: {
